0d3302516ed53a6a3182d83f15c73b5994115b2a
wysamuel <wysamuel@gmail.com>
2018-10-18 20:53:08 +0800

    bug fix

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,7 @@ from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
